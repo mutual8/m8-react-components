e6f8e05e7cd78d7903b8b0677003f3265d55a7d5
Sierk Beij <sierk@riseup.net>
2018-11-09 11:42:05 -0800

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "import",
+    "jsx-a11y",
+    "react"
+  ],
+  "rules": {
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# M8-REACT-COMPONENTS
+
+Basic component library for Mutual8 React apps.
+
+## NOTICE
+The code in this library is in the early stages of development, not yet usable, and
+frequent API changes will occur.
